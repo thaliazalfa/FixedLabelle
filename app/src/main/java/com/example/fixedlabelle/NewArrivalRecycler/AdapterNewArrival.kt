c7826faed4172ef45dbc0e655a7e42eced44ab11
thal <thalia.zw@gmail.com>
2023-01-06 08:54:37 +0700

    import adapter

--- app/src/main/java/com/example/fixedlabelle/NewArrivalRecycler/AdapterNewArrival.kt
+++ app/src/main/java/com/example/fixedlabelle/NewArrivalRecycler/AdapterNewArrival.kt
@@ -1,4 +1,14 @@
 package com.example.fixedlabelle.NewArrivalRecycler
 
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.fixedlabelle.DetailActivity
+import com.example.fixedlabelle.R
+
 class AdapterNewArrival {
 }
